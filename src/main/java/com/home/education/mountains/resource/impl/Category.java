e16a32aed28fb2836a0b997115dee020101f6c92
RomanMalachivskyi <RomanMalachivsky@gmail.com>
2015-07-20 21:48:32 +0300

    added resource

---
+++ src/main/java/com/home/education/mountains/resource/impl/Category.java
@@ -0,0 +1,66 @@
+package com.home.education.mountains.resource.impl;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "category" )
+public class Category extends GenericResourceImpl implements Serializable {
+
+	private static final long serialVersionUID = -6737556781595570134L;
+
+	private int id;
+	private String name;
+	private String description;
+
+	public Category() {
+	}
+	
+	public Category(int id, String name, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+	}
+
+	public Category(String name, String description) {
+		super();
+		this.name = name;
+		this.description = description;
+	}
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Column(name = "name", unique = true, nullable = false)
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Column(name = "description")
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
