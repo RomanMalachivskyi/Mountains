3178d5f2197d3b44ace105e99feddccdd7c7f0ed
RomanMalachivskyi <RomanMalachivsky@gmail.com>
2015-08-30 10:54:40 +0300

    improved aspects

--- src/main/java/com/home/education/mountains/aop/LogAspect.java
+++ src/main/java/com/home/education/mountains/aop/LogAspect.java
@@ -1,5 +1,7 @@
 package com.home.education.mountains.aop;
 
+import java.util.Arrays;
+
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -12,9 +14,32 @@ import org.springframework.context.annotation.Configuration;
 public class LogAspect {
 
 	private static final Logger log = LoggerFactory.getLogger(LogAspect.class);
-	
+
 	@Before("execution(* com.home.education.mountains.service.impl.ReadWriteGenericServiceImpl.*(..))")
-	public void logBefore(JoinPoint joinPoint){
-		log.info(joinPoint.getSignature().getName()+"==================");
-	} 
+	public void logBeforeWrite(JoinPoint joinPoint) {
+		buildMessage(joinPoint);
+	}
+
+	@Before("execution(* com.home.education.mountains.service.impl.ReadGenericServiceImpl.*(*))")
+	public void logBeforeRead(JoinPoint joinPoint) {
+		buildMessage(joinPoint);
+	}
+	
+	@Before("execution(* com.home.education.mountains.dao.impl.ReadGenericDaoImpl.*(*))")
+	public void logBeforeDaoRead(JoinPoint joinPoint) {
+		buildMessage(joinPoint);
+	}
+	
+	@Before("execution(* com.home.education.mountains.dao.impl.ReadWriteGenericDaoImpl.*(*))")
+	public void logBeforeDaoWrite(JoinPoint joinPoint) {
+		buildMessage(joinPoint);
+	}
+	private void buildMessage(JoinPoint joinPoint) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(joinPoint.getSourceLocation().getWithinType().getSimpleName() + " ");
+		builder.append(joinPoint.getSignature().getName() + " ");
+		Arrays.stream(joinPoint.getArgs()).forEach(signatureArg -> builder.append(signatureArg));
+		log.info(builder.toString());
+	}
+
 }
--- src/main/java/com/home/education/mountains/dao/impl/ReadGenericDaoImpl.java
+++ src/main/java/com/home/education/mountains/dao/impl/ReadGenericDaoImpl.java
@@ -2,9 +2,6 @@ package com.home.education.mountains.dao.impl;
 
 import java.util.Collection;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.home.education.mountains.common.CustomHibernateDaoSupport;
 import com.home.education.mountains.dao.ReadGenericDao;
 import com.home.education.mountains.resource.GenericResource;
@@ -12,7 +9,6 @@ import com.home.education.mountains.resource.GenericResource;
 public class ReadGenericDaoImpl<R extends GenericResource> extends CustomHibernateDaoSupport
 		implements ReadGenericDao<R> {
 
-	private static final Logger LOG = LoggerFactory.getLogger(ReadGenericDaoImpl.class);
 	protected final String tableName;
 
 	public ReadGenericDaoImpl(final String tableName) {
@@ -22,7 +18,6 @@ public class ReadGenericDaoImpl<R extends GenericResource> extends CustomHiberna
 	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<R> getById(int id) {
-		LOG.info("get " + tableName + " by id:" + id);
 		return (Collection<R>) getHibernateTemplate()
 				.find("from " + tableName + " where " + tableName.toLowerCase() + "Id=?", id);
 	}
@@ -30,7 +25,6 @@ public class ReadGenericDaoImpl<R extends GenericResource> extends CustomHiberna
 	@SuppressWarnings("unchecked")
 	@Override
 	public Collection<R> getAll() {
-		LOG.info("get all" + tableName + "s");
 		return (Collection<R>) getHibernateTemplate().find("from " + tableName);
 	}
 
